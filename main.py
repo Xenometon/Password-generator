0b5024abc4c05dfcc2c218dc4b4136dd366c2ba1
Cybernaut <106739081+Xenometon@users.noreply.github.com>
2023-03-18 01:08:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-#Python program to generate strong passwords of upto 8 characters.
+#Python program to generate strong passwords upto 70 characters
 
 import random
 lower= "abcdefghijklmnopqrstuvwxyz"
 upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numeral= "123456789"
-notation="αβΔπλΨΩ"
-symbols="@#%*&/"
-case=lower+upper+numeral+notation+symbols
-length=8
-passcode="".join(random.sample(case, length))
-print("Your new passcode is: " + passcode)
+numeral= "0123456789"
+symbols="@#*&/$~!"
+case=lower+upper+numeral+symbols
 
+try:
+   length=int(input("Enter the number of required characters (max=70) : "))
+
+   if 4<= length <=70:
+     passcode="".join(random.sample(case, length))
+     print("Your new passcode is: " + passcode)
+   elif length <4:
+     print("The minimum character length must be greater than or equal to 4.")
+   else:
+     print("Character Length too large!")
+except ValueError:
+    print("Only integral input is valid.")
+
+#End of the program
